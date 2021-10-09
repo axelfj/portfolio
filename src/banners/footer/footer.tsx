16eb6147b57c6b48b1949a51452c63c75ca391cc
Axel Fernández <axl1210@estudiantec.cr>
2021-10-08 21:23:23 -0600

    all in

---
+++ src/banners/footer/footer.tsx
@@ -0,0 +1,22 @@
+const footer = () => {
+  return (
+    <div
+      style={{
+        verticalAlign: "bottom",
+        backgroundColor: "black",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        position: "fixed",
+        height: "40px",
+        width: "100%",
+        bottom: "0",
+        color: "white",
+      }}
+    >
+      <p>Copy</p>
+    </div>
+  );
+};
+
+export default footer;
